34e4bc80499cb45923c74de58ebde9f3fbb76cca
Abaljerind <abaljerinda@gmail.com>
2024-06-20 17:48:26 +0700

    add method form.reset()

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
   submitForm.addEventListener("submit", function (event) {
     event.preventDefault();
     addBook();
+    submitForm.reset();
   });
 });
 
